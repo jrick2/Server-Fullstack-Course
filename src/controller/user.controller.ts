826b0bfe2dd649b7b8b96fe218a5d5d55b61b7cd
jrick2 <joshrick16.abellera@gmail.com>
2023-02-26 21:07:11 -0600

    server

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,92 @@
+import { Request, Response } from "express";
+import config from "config";
+import { CreateUserInput } from "../schema/user.schema";
+import { createSession } from "../service/session.service";
+import { createUser } from "../service/user.service";
+import { signJwt } from "../utils/jwt.utils";
+import logger from "../utils/logger";
+import { stripe } from "../utils/stripe";
+
+export async function createUserHandler(
+  req: Request<{}, {}, CreateUserInput["body"]>,
+  res: Response
+) {
+  try {
+    const user = await createUser(req.body);
+
+    const session = await createSession(user._id, req.get("user-agent") || "");
+
+    // create an access token
+
+    const accessToken = signJwt(
+      { ...user, session: session._id },
+      { expiresIn: config.get("accessTokenTtl") } // 15 minutes
+    );
+
+    // create a refresh token
+    const refreshToken = signJwt(
+      { ...user, session: session._id },
+      { expiresIn: config.get("refreshTokenTtl") } // 15 minutes
+    );
+
+    // return access & refresh tokens
+
+    res.cookie("accessToken", accessToken, {
+      maxAge: 900000, // 15 mins
+      httpOnly: true,
+      domain: "localhost",
+      path: "/",
+      sameSite: "strict",
+      secure: false,
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      maxAge: 3.154e10, // 1 year
+      httpOnly: true,
+      domain: "localhost",
+      path: "/",
+      sameSite: "strict",
+      secure: false,
+    });
+
+    const sucess = "User created successfully";
+
+    return res.status(201).json({
+      sucess,
+      data: {
+        _id: user._id,
+        email: user.email,
+        stripeCustomerId: user.stripeCustomerId,
+        accessToken,
+        refreshToken,
+      },
+    });
+  } catch (e: any) {
+    logger.error(e);
+    return res.status(409).send(e.message);
+  }
+}
+
+export async function getCurrentUser(req: Request, res: Response) {
+  const email = res.locals.user.email;
+  const id = res.locals.user._id;
+  const name = res.locals.user.name;
+  const createdAt = res.locals.user.createdAt;
+  const updataAt = res.locals.user.updataAt;
+  const session = res.locals.user.session;
+  const stripeCustomerId = res.locals.user.stripeCustomerId;
+
+  return res.json({
+    data: {
+      user: {
+        id,
+        email,
+        name,
+        stripeCustomerId,
+        createdAt,
+        updataAt,
+        session,
+      },
+    },
+  });
+}
---
+++ src/routes.ts
@@ -0,0 +1,44 @@
+import { Express, Request, Response } from "express";
+import { articleSession } from "./controller/article";
+
+import {
+  createUserSessionHandler,
+  getUserSessionsHandler,
+  deleteSessionHandler,
+} from "./controller/session.controller";
+import { stripeHandler, stripeSession } from "./controller/stripe";
+import {
+  createUserHandler,
+  getCurrentUser,
+} from "./controller/user.controller";
+import requireUser from "./middleware/requireUser";
+import validateResource from "./middleware/validateResource";
+
+import { createSessionSchema } from "./schema/session.schema";
+import { createUserSchema } from "./schema/user.schema";
+
+function routes(app: Express) {
+  app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+
+  app.post("/api/users", validateResource(createUserSchema), createUserHandler);
+
+  app.get("/api/me", requireUser, getCurrentUser);
+
+  app.post(
+    "/api/login",
+    validateResource(createSessionSchema),
+    createUserSessionHandler
+  );
+
+  app.get("/api/login", requireUser, getUserSessionsHandler);
+
+  app.delete("/api/login", requireUser, deleteSessionHandler);
+
+  app.get("/api/prices", requireUser, stripeHandler);
+
+  app.post("/api/session", requireUser, stripeSession);
+
+  app.get("/api/articles", requireUser, articleSession);
+}
+
+export default routes;
