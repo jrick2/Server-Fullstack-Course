488772d7275366602afa585d154de99832e6dc89
jrick2 <joshrick16.abellera@gmail.com>
2023-02-28 03:11:03 -0600

    render

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://client-course.onrender.com/"],
+    origin: ["https://client-course.onrender.com/", "http://localhost:3000/"],
     credentials: true,
   })
 );
